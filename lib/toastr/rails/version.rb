e9a298e800b3467523eac1022be316231c2e8020
Toshiyuki HINA <thina@yourinventit.com>
2019-03-14 11:51:24 +0900

    initial commit

---
+++ lib/toastr/rails/version.rb
@@ -0,0 +1,5 @@
+module Toastr
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/toastr/rails_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Toastr::Rails do
+  it "has a version number" do
+    expect(Toastr::Rails::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
